038a5d05903a4496b111e2202f71f62083e978e6
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-03-23 22:15:02 +0700

    Tạo mới Câu 1
    Tiến hành tạo Project và thêm câu 1

---
+++ ntu/NguyenKhacDuyHung_63132095/Cau1_AppBMI.java
@@ -0,0 +1,42 @@
+package NguyenKhacDuyHung_63132095;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Cau1_AppBMI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Cau1_AppBMI frame = new Cau1_AppBMI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Cau1_AppBMI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
