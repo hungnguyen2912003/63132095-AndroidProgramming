ca54418078282c22603c00603def3be43a54a0e7
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-03-24 14:26:36 +0700

    Tạo mới project Câu 1 - Thi giữa kỳ
    Tạo mới project Câu 1 - Thi giữa kỳ

---
+++ Cau1_AppBMI/ntu/NguyenKhacDuyHung_63132095/AppBMI_UI.java
@@ -0,0 +1,49 @@
+package ntu.NguyenKhacDuyHung_63132095;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class AppBMI_UI extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppBMI_UI frame = new AppBMI_UI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AppBMI_UI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(AppBMI_UI.class.getResource("/images/background4.png")));
+		lblNewLabel.setBounds(0, 0, 1000, 600);
+		contentPane.add(lblNewLabel);
+	}
+}
