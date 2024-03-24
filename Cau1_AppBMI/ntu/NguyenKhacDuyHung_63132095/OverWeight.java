a84970891c3a4308a3a3105ead33a2db69676ee2
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-03-24 20:21:10 +0700

    Câu 1. Thêm JFrame giao diện cho thừa cân
    Câu 1. Thêm JFrame giao diện cho thừa cân

---
+++ Cau1_AppBMI/ntu/NguyenKhacDuyHung_63132095/OverWeight.java
@@ -0,0 +1,58 @@
+package ntu.NguyenKhacDuyHung_63132095;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class OverWeight extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					OverWeight frame = new OverWeight();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public OverWeight() {
+		setTitle("Ứng dụng kiểm tra chỉ số BMI");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1280, 720);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(165, 10, 948, 62);
+		panel.setOpaque(false);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel bgImage = new JLabel("");
+		bgImage.setIcon(new ImageIcon(OverWeight.class.getResource("/images/ThuaCan.png")));
+		bgImage.setBounds(0, 0, 1266, 683);
+		contentPane.add(bgImage);
+	}
+
+}
