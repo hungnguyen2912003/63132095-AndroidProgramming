82e80ab0a722a007cfa877cdf51fb4a1e317bc67
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-03-25 21:37:07 +0700

    Tạo các animation chuyển cảnh giữa 2 activities
    Tạo các animation chuyển cảnh giữa 2 activities

--- Cau3_IntroduceMyself/app/src/main/java/ntu/NguyenKhacDuyHung_63132095/MainActivity.java
+++ Cau3_IntroduceMyself/app/src/main/java/ntu/NguyenKhacDuyHung_63132095/MainActivity.java
@@ -1,6 +1,11 @@
 package ntu.NguyenKhacDuyHung_63132095;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
