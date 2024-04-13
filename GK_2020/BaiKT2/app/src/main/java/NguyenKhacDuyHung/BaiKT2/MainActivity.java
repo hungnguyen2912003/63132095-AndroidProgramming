a93196d4d94f3f4480bd00593032aced23c00ef4
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-04-13 13:32:42 +0700

    Reupload Đề thi Giữa kỳ năm 2020
    Reupload Đề thi Giữa kỳ năm 2020

---
+++ GK_2020/BaiKT2/app/src/main/java/NguyenKhacDuyHung/BaiKT2/MainActivity.java
@@ -0,0 +1,59 @@
+package NguyenKhacDuyHung.BaiKT2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnTinh;
+    RadioButton rdb13, rdb15, rdb18;
+    EditText edtNum;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimDieuKhien();
+
+        btnTinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strNum = edtNum.getText().toString();
+                int num = Integer.parseInt(strNum);
+                float value;
+                float tip;
+                if(rdb13.isChecked()){
+                    value = 0.13f;
+                    tip = num * value;
+                    Toast.makeText(MainActivity.this, "Your tip will be " + tip, Toast.LENGTH_LONG).show();
+                }
+                if(rdb15.isChecked()){
+                    value = 0.15f;
+                    tip = num * value;
+                    Toast.makeText(MainActivity.this, "Your tip will be " + tip, Toast.LENGTH_LONG).show();
+                }
+                if(rdb18.isChecked()){
+                    value = 0.18f;
+                    tip = num * value;
+                    Toast.makeText(MainActivity.this, "Your tip will be " + tip, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    void TimDieuKhien(){
+        edtNum = findViewById(R.id.edtNum);
+        btnTinh = findViewById(R.id.btnTinh);
+        rdb13 = findViewById(R.id.rdb13);
+        rdb15 = findViewById(R.id.rdb15);
+        rdb18 = findViewById(R.id.rdb18);
+    }
+}
---
+++ GK_2020/BaiKT4/app/src/main/java/NguyenKhacDuyHung/BaiKT4/adapter/AnimalAdapter.java
@@ -0,0 +1,82 @@
+package NguyenKhacDuyHung.BaiKT4.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import NguyenKhacDuyHung.BaiKT4.R;
+import NguyenKhacDuyHung.BaiKT4.model.Animal;
+
+public class AnimalAdapter extends BaseAdapter {
+    ArrayList<Animal> ds;
+    Context mContext;
+    LayoutInflater mInflater;
+
+    public AnimalAdapter(Context mContext, ArrayList<Animal> ds) {
+        this.ds = ds;
+        this.mContext = mContext;
+        mInflater = LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public int getCount() {
+        return ds.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return ds.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        AnimalViewHolder viewItem;
+        if(convertView == null){
+            convertView = mInflater.inflate(R.layout.animal_item, null);
+            viewItem = new AnimalViewHolder();
+            viewItem.imageAnimal = convertView.findViewById(R.id.imgAnimal);
+            viewItem.tvName = convertView.findViewById(R.id.tvName);
+            viewItem.tvDetail = convertView.findViewById(R.id.tvDetail);
+        }
+        else{
+            viewItem = (AnimalViewHolder)convertView.getTag();
+        }
+
+        Animal animalShow = ds.get(position);
+        String tenThu = animalShow.getTen();
+        String chiTiet = animalShow.getDetail();
+        String anhThu = animalShow.getImage();
+        viewItem.tvName.setText(tenThu);
+        viewItem.tvDetail.setText(chiTiet);
+
+        int idPic = FindIDImg_NameFile(anhThu);
+
+        viewItem.imageAnimal.setImageResource(idPic);
+
+        return convertView;
+    }
+
+    //Lấy ảnh
+    int FindIDImg_NameFile(String File){
+        String tenPkg = mContext.getPackageName();
+        int id = mContext.getResources().getIdentifier(File, "mipmap", tenPkg);
+        return id;
+    }
+
+    static class AnimalViewHolder{
+        ImageView imageAnimal;
+        TextView tvName;
+        TextView tvDetail;
+    }
+}
