c2e9d9d55f6add28a53c8853309e3f1d48bb0d0a
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-04-19 20:57:06 +0700

    Tạo mới SubActivity

---
+++ Intent_VD1_ExplicitIntents/app/src/main/java/ntu/nguyenkhacduyhung/intent_vd1_explicitintents/SubActivity.java
@@ -0,0 +1,18 @@
+package ntu.nguyenkhacduyhung.intent_vd1_explicitintents;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
