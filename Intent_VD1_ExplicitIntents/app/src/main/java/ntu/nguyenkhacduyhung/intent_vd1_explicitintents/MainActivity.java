2968858f071944e4d68bb4b1355747cb17d7f84a
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-04-19 21:14:17 +0700

    Hoàn tất sự kiện button Chuyển trên Màn hình trang chủ

--- Intent_VD1_ExplicitIntents/app/src/main/java/ntu/nguyenkhacduyhung/intent_vd1_explicitintents/MainActivity.java
+++ Intent_VD1_ExplicitIntents/app/src/main/java/ntu/nguyenkhacduyhung/intent_vd1_explicitintents/MainActivity.java
@@ -1,6 +1,9 @@
 package ntu.nguyenkhacduyhung.intent_vd1_explicitintents;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnChuyen = findViewById(R.id.btnChuyen);
+
+        btnChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chuyenMan = new Intent(MainActivity.this, SubActivity.class);
+
+                startActivity(chuyenMan);
+            }
+        });
+
     }
 }
