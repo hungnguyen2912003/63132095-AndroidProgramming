fe5ed4b01cfab34e8f769b8008483617baf508f8
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-04-19 23:13:20 +0700

    Hoàn tất sự kiện nút và hiển thị UserName trên ActivityHome

--- Intent_VD2_Login/app/src/main/java/ntu/nguyenkhacduyhung/intent_vd2_login/ActivityHome.java
+++ Intent_VD2_Login/app/src/main/java/ntu/nguyenkhacduyhung/intent_vd2_login/ActivityHome.java
@@ -1,6 +1,11 @@
 package ntu.nguyenkhacduyhung.intent_vd2_login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ActivityHome extends AppCompatActivity {
 
+    EditText edtUserName = findViewById(R.id.edtUserName);
+    TextView tvUserName = findViewById(R.id.tvUserName);
+    Button btnQuayLai = findViewById(R.id.btnQuaylai);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        String strUserName = edtUserName.getText().toString();
+
+        tvUserName.setText(strUserName);
+
+        btnQuayLai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iQuayLai = new Intent(ActivityHome.this, MainActivity.class);
+
+                startActivity(iQuayLai);
+            }
         });
     }
 }
