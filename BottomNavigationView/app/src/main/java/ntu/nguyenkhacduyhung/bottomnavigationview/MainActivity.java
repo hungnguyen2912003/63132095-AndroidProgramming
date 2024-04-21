9bd1c0008da0432c1ee8419bb8abd64754a2f45e
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-04-21 15:35:10 +0700

    Điều chỉnh lại số lượng item Menu

--- BottomNavigationView/app/src/main/java/ntu/nguyenkhacduyhung/bottomnavigationview/MainActivity.java
+++ BottomNavigationView/app/src/main/java/ntu/nguyenkhacduyhung/bottomnavigationview/MainActivity.java
@@ -1,12 +1,13 @@
 package ntu.nguyenkhacduyhung.bottomnavigationview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
