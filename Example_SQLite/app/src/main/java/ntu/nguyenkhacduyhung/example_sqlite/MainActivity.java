559dddd461b14d2fc257e570439a5290928f298e
Hung Nguyen <64821688+hungnguyen2912003@users.noreply.github.com>
2024-05-05 20:52:27 +0700

    Hoàn thành hiển thị các bản ghi lên ListView
    Hoàn thành hiển thị các bản ghi lên ListView

--- Example_SQLite/app/src/main/java/ntu/nguyenkhacduyhung/example_sqlite/MainActivity.java
+++ Example_SQLite/app/src/main/java/ntu/nguyenkhacduyhung/example_sqlite/MainActivity.java
@@ -1,154 +1,91 @@
 package ntu.nguyenkhacduyhung.example_sqlite;
 
-import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.nio.file.attribute.FileOwnerAttributeView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edtID, edtName, edtPage, edtPrice, edtDes;
-
-    Button btnThem, btnSua, btnXoa, btnTVan;
-
     ListView lv;
 
-    ArrayList<String> mylist;
-    ArrayAdapter<String> adapter;
+    ArrayList<Book> mylist;
 
     SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TimDieuKhien();
-        // Tạo và mở Cơ sở dữ liệu Sqlite
-        db = openOrCreateDatabase("QLSach.db",MODE_PRIVATE,null);
-        // Tạo Table để chứa dữ liệu
-        String sqlCreateTable = "CREATE TABLE Books(BookID integer PRIMARY KEY, BookName text, Page integer, Price Float, Description text);";
+
+        lv = findViewById(R.id.lvBook);
+        //Bước 0. Tạo file cơ sở dữ liệu
+        db = openOrCreateDatabase("QLSach.db", MODE_PRIVATE,null);
+
+        //Bước 1. Tạo bảng
+            //Câu lệnh tạo bảng
         String sqlDeleteTableExist = "DROP TABLE IF EXISTS Books;\n";
+        String sqlCreateTable = "CREATE TABLE Books(BookID integer PRIMARY KEY, BookName text, Page integer, Price Float, Description text);";
+
+        //Thực hiện lệnh SQL
         db.execSQL(sqlDeleteTableExist);
         db.execSQL(sqlCreateTable);
 
+        //Thêm một số dòng dữ liệu vào bảng
+        String sqlInsertTable1 = "INSERT INTO Books VALUES(1, 'Java', 100, 9.99, 'Sách về java');";
+        String sqlInsertTable2 = "INSERT INTO Books VALUES(2, 'Android', 320, 19.00, 'Android cơ bản');";
+        String sqlInsertTable3 = "INSERT INTO Books VALUES(3, 'Học làm giàu', 120, 0.99, 'Sách đọc cho vui');";
+        String sqlInsertTable4 = "INSERT INTO Books VALUES(4, 'Tử điển Anh-Việt', 1000, 29.50, 'Từ điển 100.000 từ');";
+        String sqlInsertTable5 = "INSERT INTO Books VALUES(5, 'CNXH', 650, 50.32, 'chuyện cổ tích');";
+        String sqlInsertTable6 = "INSERT INTO Books VALUES(6, 'Con vịt', 1000, 24.35, 'Vịt vàng trong hồ bơi');";
+        String sqlInsertTable7 = "INSERT INTO Books VALUES(7, 'Mạng máy tính', 540, 86.42, 'Bài giảng Packet Tracer');";
+        String sqlInsertTable8 = "INSERT INTO Books VALUES(8, 'Ba con mèo', 1000, 100.25, 'Con mèo méo meo');";
+        String sqlInsertTable9 = "INSERT INTO Books VALUES(9, 'Năm con cá', 40, 56.42, 'Tui là con cá');";
+        String sqlInsertTable10 = "INSERT INTO Books VALUES(10, 'SQLite', 500, 150.32, 'SQLite tổng hợp');";
+        db.execSQL(sqlInsertTable1);
+        db.execSQL(sqlInsertTable2);
+        db.execSQL(sqlInsertTable3);
+        db.execSQL(sqlInsertTable4);
+        db.execSQL(sqlInsertTable5);
+        db.execSQL(sqlInsertTable6);
+        db.execSQL(sqlInsertTable7);
+        db.execSQL(sqlInsertTable8);
+        db.execSQL(sqlInsertTable9);
+        db.execSQL(sqlInsertTable10);
+        //Để quan sát trực quan file .db => Dùng ứng dụng
+            //Đóng lại để check
+        db.close();
 
-        btnThem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int id = Integer.parseInt(edtID.getText().toString());
-                String name = edtName.getText().toString();
-                int page = Integer.parseInt(edtPage.getText().toString());
-                float price = Float.parseFloat(edtPrice.getText().toString());
-                String des = edtDes.getText().toString();
+        ////////////////////----Hiển thị lên Android---/////////////////////
+        //Bước 1: Mở CSDL
+        SQLiteDatabase db = openOrCreateDatabase("QLSach.db", MODE_PRIVATE,null);
 
-                ContentValues myvalue = new ContentValues();
-                myvalue.put("BookID", id);
-                myvalue.put("BookName", name);
-                myvalue.put("Page", page);
-                myvalue.put("Price", price);
-                myvalue.put("Description", des);
-                String msg = "";
-                if(db.insert("Books", null, myvalue) == -1){
-                    msg = "Thêm dữ liệu thất bại";
-                }
-                else{
-                    msg = "Thêm dữ liệu thành công";
-                }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-                edtID.setText("");
-                edtName.setText("");
-                edtPage.setText("");
-                edtPrice.setText("");
-                edtDes.setText("");
-                edtID.requestFocus();
-            }
-        });
+        //Bước 2: Thực thi câu lệnh Select
+        String sqlSelect = "Select * FROM Books";
+        Cursor cs =  db.rawQuery(sqlSelect, null);
 
-        btnXoa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int id = Integer.parseInt(edtID.getText().toString());
-                int n = db.delete("Books", "id = ?", new String[]{String.valueOf(id)});
-                String msg = "";
-                if(n == 0){
-                    msg = "Không có dữ liệu để xoá";
-                }
-                else{
-                    msg = "Bản ghi " + n + "đã được xoá";
-                }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-            }
-        });
+        //Buước 3: Đổ vào biến nào đó để xử lý
+            //3.1. Xây dựng model/class cho bảng Book. Ví dụ Book.java
+            //3.2. Tạo biến ArrayList<Book> dsSach
+        mylist = new ArrayList<Book>();
+            //3.3. Duyệt lần lượt từng bảng ghi và thêm vào danhSach
+        while (cs.moveToNext()){
+            int idSach = cs.getInt(0);
+            String tenSach = cs.getString(1);
+            int soTrang = cs.getInt(2);
+            float gia = cs.getFloat(3);
+            String mota = cs.getString(4);
 
-        btnSua.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int id = Integer.parseInt(edtID.getText().toString());
-                String name = edtName.getText().toString();
-                int page = Integer.parseInt(edtPage.getText().toString());
-                float price = Float.parseFloat(edtPrice.getText().toString());
-                String des = edtDes.getText().toString();
-                ContentValues myvalue = new ContentValues();
-                myvalue.put("BookName", name);
-                myvalue.put("Page", page);
-                myvalue.put("Price", price);
-                myvalue.put("Description", des);
-                int n = db.delete("Books", "id = ?", new String[]{String.valueOf(id)});
-                String msg = "";
-                if(n == 0){
-                    msg = "Không có dữ liệu để xoá";
-                }
-                else{
-                    msg = "Bản ghi " + n + "đã được cập nhật";
-                }
-                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
-            }
-        });
+            // Tạo một đối tượng sách và thêm vào danh sách
+            Book b = new Book(idSach,tenSach,soTrang,gia,mota);
+            mylist.add(b);
+        }
 
-        btnTVan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mylist.clear();
-                Cursor c = db.query("Books", null, null,null,null,null, null);
-                c.moveToNext();
-                String data = "";
-                while(c.isAfterLast() == false){
-                    data = c.getString(0) + " - " + c.getString(1) + " - " + c.getString(2);
-                    c.moveToNext();
-                    mylist.add(data);
-                }
-                c.close();
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    void TimDieuKhien(){
-        edtID = findViewById(R.id.edtBookID);
-        edtName = findViewById(R.id.edtBookName);
-        edtPage = findViewById(R.id.edtPage);
-        edtPrice = findViewById(R.id.edtGia);
-        edtDes = findViewById(R.id.edtDescription);
-
-        btnThem = findViewById(R.id.btnThem);
-        btnSua = findViewById(R.id.btnUpdate);
-        btnXoa = findViewById(R.id.btnDel);
-        btnTVan = findViewById(R.id.btnQuery);
-
-        lv = findViewById(R.id.lvBook);
-
-        mylist = new ArrayList<>();
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mylist);
+        BookAdapter adapter = new BookAdapter(this, mylist);
 
         lv.setAdapter(adapter);
     }
